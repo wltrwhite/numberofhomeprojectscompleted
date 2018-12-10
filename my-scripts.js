34d19beb5383b6f3bb894ab21a02586d9985797e
wltrwhite <45766344+wltrwhite@users.noreply.github.com>
2018-12-10 15:51:53 -0700

    Add files via upload

---
+++ my-scripts.js
@@ -0,0 +1,25 @@
+// set count
+if(localStorage.getItem("count") == null){
+	localStorage.setItem("count",0)
+}
+
+// update count
+function update(){
+	document.getElementById("count").innerHTML = localStorage.getItem("count")
+}
+
+update()
+
+// add one to count
+function plusOne(){
+	localStorage.setItem("count",Number(localStorage.getItem("count")) + 1)
+	update()
+}
+
+// reset count
+function reset(){
+	if (confirm("Are you sure you want to eliminate your high score?")){
+		localStorage.setItem("count",0)
+			update()
+	}
+}
